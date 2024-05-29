44327abe981bc578ace8100bf32bb3b4f8c9218d
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-29 13:05:55 -0700

    first commit

---
+++ question 21/main.ts
@@ -0,0 +1,21 @@
+// question 21
+
+let objectContainingItems : {[key : string]:  any} = {
+    "laptop":{
+        name: "Laptop",
+        price: "50000",
+        brand: "Dell"
+    },
+    "mouse": {
+        name: "mouse",
+        price: "2000",
+        brand: "Dell"
+    },
+    "keyBoard":{
+        name: "key board",
+        price: "3000",
+        brand: "Dell"
+    },
+
+};
+console.log(objectContainingItems);
---
+++ question 22/main.ts
@@ -0,0 +1,11 @@
+// question 21
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet,  try to make one happen.
+ Change an index in one of your programs to produce an index error.Make sure you correct the error before closing the program.*/
+
+let intentionalError:number[] = [3,4,5,8,9,4];
+console.log(intentionalError[10]);
+
+
+
+let intentionError:number[] = [3,4,5,8,9];
+console.log(intentionError[2]);
