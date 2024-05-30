a3acb74b83559a971fa85ecdc2c2a8142d2538d5
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-30 12:02:36 -0700

    first commit

---
+++ question 26/main.js
@@ -0,0 +1,17 @@
+// question 26
+var alienColor = ("green");
+if (alienColor === "green") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
+;
+// second version
+if (alienColor === "blue") {
+    console.log("I am come from IF statment");
+}
+else {
+    console.log("I am come from ELSE statment");
+}
+;
---
+++ question 26/main.ts
@@ -0,0 +1,20 @@
+// question 26
+
+let alienColor :string = ("green");
+
+if (alienColor === "green") {
+    console.log("the player just earned 5 points for shooting the alien.");
+
+}
+else{
+    console.log("the player just earned 10 points.");
+};
+
+// second version
+if (alienColor === "blue"){
+    console.log("I am come from IF statment");
+}
+
+else{
+    console.log("I am come from ELSE statment");
+};
