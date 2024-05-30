8e9daed15d32ec9c67cfb0a8565665b29f78e53c
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-30 12:27:46 -0700

    first commit

---
+++ question 27/main.js
@@ -0,0 +1,34 @@
+// question 27
+// usinf if and else if
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log(" shot down green alien the player earned 5 points");
+}
+else if (alienColor === "yellow") {
+    console.log(" shot down yellow alien the player earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log(" shot down red alien the player earned 15 points.");
+}
+// second version
+var alienColor1 = "yellow";
+if (alienColor1 === "green") {
+    console.log(" shot down green alien the player earned 5 points");
+}
+else if (alienColor1 === "yellow") {
+    console.log(" shot down yellow alien the player earned 10 points.");
+}
+else if (alienColor1 === "red") {
+    console.log(" shot down red alien the player earned 15 points.");
+}
+//third version
+var alienColor2 = "red";
+if (alienColor2 === "green") {
+    console.log(" shot down green alien the player earned 5 points");
+}
+else if (alienColor2 === "yellow") {
+    console.log(" shot down yellow alien the player earned 10 points.");
+}
+else if (alienColor2 === "red") {
+    console.log(" shot down red alien the player earned 15 points.");
+}
---
+++ question 27/main.ts
@@ -0,0 +1,45 @@
+// question 27
+
+// usinf if and else if
+let alienColor:string = "green";
+if (alienColor === "green"){
+    console.log(" shot down green alien the player earned 5 points")
+}
+
+
+else if (alienColor === "yellow"){
+    console.log(" shot down yellow alien the player earned 10 points.")
+}
+else if (alienColor === "red"){
+    console.log(" shot down red alien the player earned 15 points.")
+}
+
+// second version
+let alienColor1 : string ="yellow";
+
+if (alienColor1 === "green"){
+    console.log(" shot down green alien the player earned 5 points")
+}
+
+else if (alienColor1 === "yellow"){
+    console.log(" shot down yellow alien the player earned 10 points.")
+}
+
+else if (alienColor1 === "red"){
+    console.log(" shot down red alien the player earned 15 points.")
+}
+
+//third version
+let alienColor2 : string ="red";
+
+if (alienColor2 === "green"){
+    console.log(" shot down green alien the player earned 5 points")
+}
+
+else if (alienColor2 === "yellow"){
+    console.log(" shot down yellow alien the player earned 10 points.")
+}
+
+else if (alienColor2 === "red"){
+    console.log(" shot down red alien the player earned 15 points.")
+}
