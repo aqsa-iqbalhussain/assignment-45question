fa2b27ed0fa5b4e8818bce7de7ad655bd15458d7
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-30 11:40:35 -0700

    first commit

---
+++ question 25/main.ts
@@ -0,0 +1,15 @@
+// question 25
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+let alien_color : string=("green");
+
+if (alien_color === "green"){
+    console.log("the player just earned 5 points")
+};
+if (alien_color === "red"){
+    console.log("alien color is red")
+};
+
+
+
+
