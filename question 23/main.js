20b6f956bb665d684260cc4f965ab3fe3212ffb9
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-30 05:27:44 -0700

    first commit

---
+++ question 23/main.js
@@ -0,0 +1,33 @@
+// question 23
+// Conditional Tests: Write a series of conditional tests.
+// test 1
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// test 2
+console.log("Is car == 'audi'? I predict False.");
+console.log(car == 'audi');
+// test 3
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+// test 4
+console.log("Is car == 'subaru'? I predict False.");
+console.log(car == 'subaru');
+// test 5
+console.log("Is car.length > 0? I predict True.");
+console.log(car.length > 0);
+// test 6
+console.log("Is car.length < 5 ? I predict False.");
+console.log(car.length < 5);
+// test 7
+console.log("Is car.toUpperCase() === 'SUBARU' ? I predict True.");
+console.log(car.toUpperCase() === 'SUBARU');
+// test 8
+console.log("Is car.toUpperCase() === 'subaru' ? I predict True.");
+console.log(car.toUpperCase() === 'subaru');
+// test 9
+console.log("Is car.includes('t')  ? I predict True.");
+console.log(car.includes('t'));
+// test 10
+console.log("Is car.toLowerCase() === 'SUBARU' ? I predict True.");
+console.log(car.toLowerCase() === 'SUBARU');
