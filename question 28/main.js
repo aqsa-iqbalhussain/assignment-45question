56da4b0cea025666b3d995f553a66bcc05c219b6
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-05-31 00:49:57 -0700

    first commit

---
+++ question 28/main.js
@@ -0,0 +1,21 @@
+"use strict";
+// question 28
+let age = 21;
+if (age < 2) {
+    console.log("you are baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("you are toddler");
+}
+else if (age >= 4 && age < 13) {
+    console.log("you are kid");
+}
+else if (age >= 13 && age < 20) {
+    console.log("you are teenager");
+}
+else if (age >= 20 && age < 65) {
+    console.log("you are adult");
+}
+else if (age >= 65) {
+    console.log("you are elder");
+}
