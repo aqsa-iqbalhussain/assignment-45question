58b53f3d7f9398591384ae2cc45f43ebf3b3048e
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-01 06:27:43 -0700

    first commit

---
+++ question 29/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// question 29
+let favorite_fruits = ["apple", "orange", "banana"];
+if (favorite_fruits.includes("orange")) {
+    console.log("I like orange");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("I like apple");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("I like strawberry");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("I like mango");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("I like banana");
+}
---
+++ question 29/main.ts
@@ -0,0 +1,23 @@
+// question 29
+
+let favorite_fruits:string[] =["apple", "orange", "banana"]
+
+if(favorite_fruits.includes("orange")) {
+    console.log("I like orange");
+}
+
+if(favorite_fruits.includes("apple")) {
+    console.log("I like apple");
+}
+
+if(favorite_fruits.includes("strawberry")) {
+    console.log("I like strawberry");
+}
+ 
+if (favorite_fruits.includes("mango")) {
+    console.log("I like mango");
+}
+
+if (favorite_fruits.includes("banana")) {
+    console.log("I like banana");
+}
