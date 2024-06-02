490d74c2d8b5001498c9bbd2fd375144c09d165c
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-02 02:37:12 -0700

    first commit

---
+++ question 31/main.ts
@@ -0,0 +1,19 @@
+// question 31
+
+let userName = ["Ayesha","Safia","Aliza","Admin","Alisha"];
+userName=[]
+
+if(userName.length ===0) {
+    console.log("we need to find some users!")
+
+}
+else{
+    userName.forEach(name => {
+        if(name === "Admin"){
+            console.log(`Hello ${name}, would you like to see a status report?`)
+        } else{
+            console.log(`Hello ${name}, thank you for logging in again.`)
+        }
+          
+    });
+}
