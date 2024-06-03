b15a756a0e124660caba54bfee243ecf925b9f6d
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-03 05:41:10 -0700

    first commit

---
+++ question 34/main.ts
@@ -0,0 +1,14 @@
+// question 34
+
+let favourite_pizza=["chicken Tika", "Malai cheese","Fajita"];
+
+// for loop use
+for (let pizza of favourite_pizza) {
+    console.log(`I like ${pizza} pizza`)
+
+}
+
+// print a message outside of the for loop
+
+console.log('really like pizza')
+console.log("I eat pizza")
