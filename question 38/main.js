c70bdf5f6e024fd3466df3ad97e1d9a118c63bd7
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-03 07:37:34 -0700

    first commit

---
+++ question 38/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// question 38
+function describe_city(city = "karachi", country = "Pakistan") {
+    console.log(`${city} is in ${country}`);
+}
+describe_city();
+describe_city("Lahore");
+describe_city("makkah", "saudia Arabia");
---
+++ question 38/main.ts
@@ -0,0 +1,10 @@
+// question 38
+function describe_city(city: string="karachi",country: string="Pakistan"){
+    console.log(`${city} is in ${country}`)
+
+
+}
+
+describe_city();
+describe_city("Lahore");
+describe_city("makkah","saudia Arabia")
