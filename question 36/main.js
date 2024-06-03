76748854a0266c51356ee85e0cd4582ae4da4277
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-03 06:35:19 -0700

    first commit

---
+++ question 36/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// question 36
+function make_shirt(size, printMessage) {
+    console.log(`you selected ${size} size shirt with ${printMessage} print on shirt`);
+}
+make_shirt("medium", "Dr Aqsa");
---
+++ question 36/main.ts
@@ -0,0 +1,8 @@
+// question 36
+
+function make_shirt (size:string , printMessage:string){
+    console.log(`you selected ${size} size shirt with ${printMessage} print on shirt`);
+
+}
+
+make_shirt("medium","Dr Aqsa")
