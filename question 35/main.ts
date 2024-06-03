e7a394d0043b5a96be949d45997eca8263269bc0
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-03 05:57:53 -0700

    first commit

---
+++ question 35/main.ts
@@ -0,0 +1,12 @@
+// question 35
+
+let petAnimals=["Dog","Cat","Rabbit"];
+
+//use for loop
+for(let animal of petAnimals){
+    console.log(`A ${animal} would make a great pet.`)
+}
+
+
+//print the message outside of for loop
+console.log("Any of these animals would make a great pet!")
