4de53201636d198ac17678c932d4d66e35f58281
aqsaiqbal <aqsaiqbal12345678@gmail.com>
2024-06-04 01:53:56 -0700

    first commit

---
+++ question 42/main.ts
@@ -0,0 +1,23 @@
+// questions 42
+
+//Great Magicians: Start with a copy of your program from Exercise 41.
+// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name.
+// Call show_magicians() to see that the list has actually been modified.
+
+let magicianNames :string[] = ["Harry Potter","Hermione Granger","Ron weasley","Albus Dumbledore"];
+
+function make_great(magicianNames:string[]){
+    for (let i=0; i<magicianNames.length; i++){
+        magicianNames[i] = "the Great " + magicianNames[i];
+     
+        
+    };
+}
+function show_magicians(magicianNames:string[]){
+    magicianNames.forEach(name => {
+        console.log(name);
+    });
+}    
+        
+make_great(magicianNames);
+show_magicians(magicianNames)
